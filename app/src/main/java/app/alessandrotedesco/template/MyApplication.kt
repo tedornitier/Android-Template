59fb6d253cbbe5bf3257760f43236ada73607194
Alessandro Tedesco <spam.alessandrotedd@gmail.com>
2022-10-31 00:42:16 +0100

    Add hilt classes

---
+++ app/src/main/java/app/alessandrotedesco/template/MyApplication.kt
@@ -0,0 +1,11 @@
+package app.alessandrotedesco.template
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/app/alessandrotedesco/template/injection/HiltMainModule.kt
@@ -0,0 +1,10 @@
+package app.alessandrotedesco.template.injection
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HiltMainModule {
+}
