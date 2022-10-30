369342b4fce604a0aad0cc843d4827944574b719
Alessandro Tedesco <spam.alessandrotedd@gmail.com>
2022-10-31 00:43:55 +0100

    Add Hilt dependency injection

--- app/src/main/java/app/alessandrotedesco/template/injection/HiltMainModule.kt
+++ app/src/main/java/app/alessandrotedesco/template/injection/HiltMainModule.kt
@@ -1,10 +1,16 @@
 package app.alessandrotedesco.template.injection
 
+import app.alessandrotedesco.template.apiservice.RemoteDataSourceRetrofit
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object HiltMainModule {
+    @Singleton
+    @Provides
+    fun provideWebService() = RemoteDataSourceRetrofit()
 }
