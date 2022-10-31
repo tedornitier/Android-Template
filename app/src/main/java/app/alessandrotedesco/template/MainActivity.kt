7cd9ac142fcac0e57ba96f551b322b8581438352
Alessandro Tedesco <spam.alessandrotedd@gmail.com>
2022-10-31 01:56:04 +0100

    Add example viewModel

--- app/src/main/java/app/alessandrotedesco/template/MainActivity.kt
+++ app/src/main/java/app/alessandrotedesco/template/MainActivity.kt
@@ -6,15 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import app.alessandrotedesco.template.ui.navigation.Main
-import app.alessandrotedesco.template.ui.section.MainSection
+import app.alessandrotedesco.template.ui.section.main.MainSection
 import app.alessandrotedesco.template.ui.theme.TemplateTheme
 import dagger.hilt.android.AndroidEntryPoint
 
