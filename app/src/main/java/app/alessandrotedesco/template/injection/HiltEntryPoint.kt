6159d59eeb9c2d55c6ddb8469cb5bc7ffb15fad2
Alessandro Tedesco <spam.alessandrotedd@gmail.com>
2022-10-31 01:26:54 +0100

    Add Hilt dependency injection

--- app/src/main/java/app/alessandrotedesco/template/injection/HiltEntryPoint.kt
+++ app/src/main/java/app/alessandrotedesco/template/injection/HiltEntryPoint.kt
@@ -2,6 +2,7 @@ package app.alessandrotedesco.template.injection
 
 import app.alessandrotedesco.template.apiservice.RemoteDataSourceRetrofit
 import app.alessandrotedesco.template.datastore.DataStoreManager
+import app.alessandrotedesco.template.database.AppDatabase
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -11,4 +12,5 @@ import dagger.hilt.components.SingletonComponent
 interface HiltEntryPoint {
     fun webService(): RemoteDataSourceRetrofit
     fun dataStoreManager(): DataStoreManager
+    fun database(): AppDatabase
 }
