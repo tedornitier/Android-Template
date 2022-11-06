2d20239df3c44c2fe4f62a22a668a3d733a48a6d
Alessandro Tedesco <spam.alessandrotedd@gmail.com>
2022-11-06 19:57:05 +0100

    Add notification + channel in NotificationUtils

--- app/src/main/java/app/alessandrotedesco/template/ui/section/main/MainSection.kt
+++ app/src/main/java/app/alessandrotedesco/template/ui/section/main/MainSection.kt
@@ -1,20 +1,31 @@
 package app.alessandrotedesco.template.ui.section.main
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import app.alessandrotedesco.template.R
 import app.alessandrotedesco.template.apiservice.model.Pokemon
 import app.alessandrotedesco.template.ui.theme.TemplateTheme
+import app.alessandrotedesco.template.utils.createNotificationChannel
+import app.alessandrotedesco.template.utils.showNotification
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
@@ -28,9 +39,18 @@ fun MainSection(navController: NavHostController, viewModel: MainViewModel = hil
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun MainSectionUI(navController: NavHostController, pokemon: MutableLiveData<Pokemon>, getPokemon: (String) -> Unit = {}) {
+fun MainSectionUI(
+    navController: NavHostController,
+    pokemon: MutableLiveData<Pokemon>,
+    getPokemon: (String) -> Unit = {}
+) {
     val notificationsPermissionState =
         rememberPermissionState(permission = Manifest.permission.POST_NOTIFICATIONS)
+
+    val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        context.createNotificationChannel("channelId", "channelName", "channelDescription")
+    }
 
     Column {
         Text(text = pokemon.observeAsState().value?.let { it.name + " #" + it.id }
@@ -42,7 +62,13 @@ fun MainSectionUI(navController: NavHostController, pokemon: MutableLiveData<Pok
             Text(text = "Get Charizard")
         }
         if (notificationsPermissionState.status.isGranted) {
-            OutlinedButton(onClick = showNotification()) {
+            OutlinedButton(onClick = {
+                context.showNotification(
+                    title = "Title",
+                    content = "Text",
+                    channelId = "channelId"
+                )
+            }) {
                 Text(text = "Show notification")
             }
         } else {
@@ -52,10 +78,6 @@ fun MainSectionUI(navController: NavHostController, pokemon: MutableLiveData<Pok
         }
     }
     // TODO example: navController.navigate(Main.Example2.route)
-}
-
-fun showNotification(): () -> Unit {
-    TODO()
 }
 
 @Preview(showBackground = true)
